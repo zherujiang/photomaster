4faee182aa5868fdc9e4737844567573598dbe3a
Cheryl Jiang <cheryl.zjiang@gmail.com>
2022-12-18 13:11:04 -0800

    first commit

---
+++ README.md
@@ -0,0 +1,14 @@
+## Set up backend environment
+### Create virtual environment
+
+```bash
+conda create --name [project name] python=3.9.15
+conda activate [project name]
+```
+
+### Install python dependencies
+
+```bash
+cd backend
+pip install -r requirements.txt
+```
---
+++ backend/app.py
@@ -0,0 +1,31 @@
+import os
+from flask import Flask
+from models import setup_db
+from flask_cors import CORS
+
+
+def create_app(test_config=None):
+
+    app = Flask(__name__)
+    setup_db(app)
+    CORS(app)
+
+    @app.route('/')
+    def get_greeting():
+        excited = os.environ['STARTED']
+        greeting = "Hello"
+        if excited == 'true':
+            greeting = greeting + "Capstone project kicked off!."
+        return greeting
+
+    @app.route('/coolkids')
+    def be_cool():
+        return "Be cool, man, be coooool! You're almost a FSND grad!"
+
+    return app
+
+
+app = create_app()
+
+if __name__ == '__main__':
+    app.run()
