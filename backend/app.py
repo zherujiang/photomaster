45699de674af78c8b9815fd884336df740fb4126
Cheryl Jiang <cheryl.zjiang@gmail.com>
2022-12-20 00:43:22 -0800

    initialize server APIs

--- backend/app.py
+++ backend/app.py
@@ -1,52 +1,231 @@
 import os
-from flask import Flask, request
-from models import setup_db
+from flask import Flask, request, jsonify, abort, redirect, flash
+from models import setup_db, Photographer, Service, Photo, Price
 from flask_cors import CORS
+from settings import UPLOAD_FOLDER, ALLOWED_EXTENSIONS
+from werkzeug.utils import secure_filename
 
+def find_city(zip_code):
+    return "A helper function to return a city based on the zip code entered"
+
+def paginate_photographers(selection, page):
+    RESULTS_PER_PAGE = 10
+    photographers = [photographer.format() for photographer in selection]
+    start = RESULTS_PER_PAGE * page + 1
+    end = start + RESULTS_PER_PAGE
+    current_photographers = photographers[start:end]
+    return current_photographers
+
+def allowed_file(filename):
+    return '.' in filename and \
+        filename.split('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
 def create_app(test_config=None):
 
     app = Flask(__name__)
     setup_db(app)
     CORS(app)
-
+    
     @app.route('/services')
     def get_services():
-        return "This API should return a list of all the photography categories"
-
-    def find_city(zip_code):
-        return "A helper function to return a city based on the zip code entered"
+        services_query = Service.query.order_by(Service.id).all()
+        services = [service.format() for service in services_query]
+        print('service categories: ', services)
+        return jsonify({
+            'success': True,
+            'services': services
+        })
 
     # search photographers
     @app.route('/photographers')
     def get_photographers():
-        return "This API should return photographers that match the search conditions"
+        service_type = request.args.get('service')
+        location = request.args.get('location')
+        city = find_city(location)
+        current_page = request.args.get('page')
+        if service_type and city:
+            try:
+                photographer_query = Photographer.query.\
+                    filter(Photographer.services.contains(service_type)).\
+                    filter(Photographer.city==city).order_by(Photographer.name).all()
+            except:
+                abort(422)
+                
+            photographers = paginate_photographers(photographer_query, current_page)
+            return jsonify({
+                'success': True,
+                'photographer': photographers
+            })
+            
+        elif city == None:
+            return jsonify({
+                'success': False,
+                'message': 'city or zip code not valid',
+                'error': 400
+            })
+        else:
+            abort(400)        
 
-    # add photographer
+
+    # add a photographer
     @app.route('/photographers', methods=['POST'])
     def create_photographer():
-        return "This API should create a new photographer"
+        new_photographer = Photographer(name = 'nametest', email = 'emailtest')
+        new_photographer.insert()
+        return "This API should create a new photographer via third party quthentication service"
     
     # edit photographer
-    @app.route('/photographers/<int:photographer_id>', methods=['PATCH'])
+    # this endpoint requires authentication
+    @app.route('/photographers/<int:photographer_id>', methods=['GET', 'PATCH'])
     def update_photographer(photographer_id):
-        return f"This API should update the photographer {photographer_id}"
-    
+        photographer = Photographer.query.filter(Photographer.id==photographer_id).one_or_none()
+        if not photographer:
+            abort(404)
+            
+        if request.method == 'GET':
+            # load the photographer information and render the form
+            photos_query = Photo.query.filter(Photo.photographer_id==photographer_id).\
+                order_by(Photo.service_id).all()
+            prices_query = Price.query.filter(Price.photographer_id==photographer_id).\
+                order_by(Price.service_id).all()
+            photos = [photo.format() for photo in photos_query]
+            prices = [price.format() for price in prices_query]
+            
+            return jsonify({
+                'success': True,
+                'photographer': photographer.format(),
+                'photos': photos,
+                'prices': prices
+            })
+        else:
+            try:
+                # parse data from the submitted form
+                request_body = request.get_json()
+                city = request_body.get('city')
+                services = request_body.get('services')
+                address = request_body.get('address')
+                profile_photo = request_body.get('profile_photo')
+                portfolio_link = request_body.get('portfolio_link')
+                social_media = request_body.get('social_media')
+                bio = request_body.get('bio')
+                
+                photographer.city = city
+                photographer.services = services
+                photographer.address = address
+                photographer.profile_photo = profile_photo
+                photographer.portfolio_link = portfolio_link
+                photographer.social_media = social_media
+                photographer.bio = bio
+                
+                photographer.update()
+                
+                return jsonify({
+                    'success': True,
+                    'photographer updated': photographer.format()
+                })
+            except:
+                abort(422)
+        
+    # delete the photographer
+    # this endpoint requires authentication
     @app.route('/photographers/<int:photographer_id>', methods=['DELETE'])
     def delete_photographer(photographer_id):
-        return f"The photographer {photographer_id} has been deleted"
+        photographer = Photographer.query.filter(Photographer.id==photographer_id).one_or_none()
+        if not photographer:
+            abort(404)
+        else:
+            try:
+                photographer.delete()
+                return jsonify({
+                    'success': True,
+                    'photograhper deleted': photographer.id
+                })
+            except:
+                abort(422)
 
     @app.route('/photos/<int:photographer_id>')
     def get_photos(photographer_id):
-        return f"This API should return photos by the photographer {photographer_id}"
+        photographer = Photographer.query.filter(Photographer.id==photographer_id).one_or_none()
+        if not photographer:
+            abort(404)
+        try:
+            photos_query = Photo.query.filter(Photo.photographer_id==photographer_id).\
+                order_by(Photo.service_id).all()
+            if photos_query:
+                photos = [photo.format() for photo in photos_query]
+            else:
+                photos = []
+            return jsonify({
+                'success': True,
+                'photos': photos
+            })
+        except:
+            abort(500)
 
+    # add photos for a photographer
+    # this endpoint requires authentication
     @app.route('/photos/<int:photographer_id>', methods=['POST'])
     def add_photos(photographer_id):
-        return f"This API should add new photos to the gallery of photographer {photographer_id}"
-
-    @app.route('/photos/<int:photographer_id>/<int:photo_id>', methods=['DELETE'])
-    def delete_photos(photographer_id, photo_id):
-        return f"This API should delete the photo {photo_id} for the selected photographer {photographer_id}"
+        app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+        photographer = Photographer.query.filter(Photographer.id==photographer_id).one_or_none()
+        if not photographer:
+            abort(404)
+        
+        if 'file' not in request.files:
+            flash('No file part')
+            return redirect(request.url)
+        
+        file = request.files['file']
+        
+        if file.filename == '':
+            flash('No selected file')
+            return redirect(request.url)
+        
+        service_tag = request.form.get('service_tag')
+        service_id = Service.query.filter(Service.name==service_tag).one_or_none().id
+        
+        if file and allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+            image_path = os.path.join(app.config['UPLOAD_FOLDER'], filename)
+            file.save(image_path)
+            
+            try:
+                new_photo = Photo(
+                    photographer_id,
+                    service_id,
+                    image_path
+                    )
+                new_photo.insert()
+                
+                return jsonify({
+                    'success': True,
+                    'image': new_photo.format()
+                })
+            except:
+                abort(422)
+            
+        elif not allowed_file(file.filename):
+            flash('File name not allowed')
+            return redirect(request.url)
+ 
+    # delete a photo from a photographers' gallery
+    # this endpoint requires authentication
+    @app.route('/photos/<int:photographer_id>', methods=['DELETE'])
+    def delete_photos(photographer_id):
+        photographer = Photographer.query.filter(Photographer.id==photographer_id).one_or_none()
+        if not photographer:
+            abort(404)
+        image_path = request.args('image_path')
+        photo_query = Photo.query.filter(Photo.image_path==image_path).one_or_none()
+        
+        try:
+            photo_query.delete()
+            return jsonify({
+                'success': True,
+                'photo_deleted': photo_query.format()
+            })
+        except:
+            abort(422)
     
     return app
 
--- backend/models.py
+++ backend/models.py
@@ -34,7 +34,7 @@ class Photographer(db.Model):
     id = Column(Integer, primary_key=True)
     name = Column(String(120), nullable=False)
     email = Column(String(120), nullable=False)
-    city = Column(String(120), nullable=False)
+    city = Column(String(120))
     services = Column(db.ARRAY(Integer, dimensions=1))
     profile_photo = Column(String(255))
     address = Column(String(255))
@@ -48,18 +48,34 @@ class Photographer(db.Model):
     prices = db.relationship(
         'Price', back_populates='photographer', cascade='all, delete')
 
-    def __init__(self, name, email, city):
+    def __init__(self, name, email):
         self.name = name
         self.email = email
-        self.city = city
 
     def format(self):
         return {
             'id': self.id,
             'name': self.name,
-            'city': self.city
+            'email': self.email,
+            'city': self.city,
+            'address': self.address,
+            'services': self.services,
+            'profile_photo': self.profile_photo,
+            'portfolio_link': self.portfolio_link,
+            'social_media': self.social_media,
+            'bio': self.bio
         }
-
+    
+    def insert(self):
+        db.session.add(self)
+        db.session.commit()
+    
+    def update(self):
+        db.session.commit()
+    
+    def delete(self):
+        db.session.delete(self)
+        db.session.commit()
 
 class Service(db.Model):
     __tablename__ = 'services'
@@ -73,6 +89,13 @@ class Service(db.Model):
         'Photo', back_populates='service', cascade='all, delete')
     prices = db.relationship(
         'Price', back_populates='service', cascade='all, delete')
+    
+    def format(self):
+        return {
+            'id': self.id,
+            'name': self.name,
+            'image_link': self.image_link,
+        }
 
 
 class Photo(db.Model):
@@ -81,11 +104,31 @@ class Photo(db.Model):
         'photographers.id', ondelete='CASCADE'), primary_key=True)
     service_id = Column(db.ForeignKey(
         'services.id', ondelete='CASCADE'), primary_key=True)
-    image = Column(String(255), nullable=False)
+    image_path = Column(String(255), nullable=False)
 
     # relationships
     photographer = db.relationship('Photographer', back_populates='photos')
     service = db.relationship('Service', back_populates='photos')
+    
+    def __init__(self, photographer_id, service_id, image_path):
+        self.photographer_id = photographer_id
+        self.service_id = service_id
+        self.image_path = image_path
+        
+    def format(self):
+        return {
+            'photographer_id': self.photographer_id,
+            'service_id': self.service_id,
+            'image_path': self.image_path,
+        }
+    
+    def insert(self):
+        db.session.add(self)
+        db.session.commit()
+    
+    def delete(self):
+        db.session.delete(self)
+        db.session.commit()
 
 
 class Price(db.Model):
@@ -99,3 +142,23 @@ class Price(db.Model):
     # relationships
     photographer = db.relationship('Photographer', back_populates='prices')
     service = db.relationship('Service', back_populates='prices')
+
+    def __init__(self, photographer_id, service_id, price):
+        self.photographer_id = photographer_id
+        self.service_id = service_id
+        self.price = price
+
+    def format(self):
+        return {
+            'photographer_id': self.photographer_id,
+            'service_id': self.service_id,
+            'price': self.price,
+        }
+    
+    def insert(self):
+        db.session.add(self)
+        db.session.commit()
+    
+    def delete(self):
+        db.session.delete(self)
+        db.session.commit()
