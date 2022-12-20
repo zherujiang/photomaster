494361a0e214049dca549b72bd9a73503874c79e
Cheryl Jiang <cheryl.zjiang@gmail.com>
2022-12-20 00:45:33 -0800

    add upload configuration to settings

--- backend/settings.py
+++ backend/settings.py
@@ -4,3 +4,5 @@ load_dotenv()
 DB_NAME = os.environ.get('DB_NAME')
 DB_USER = os.environ.get('DB_USER')
 DB_PASSWORD = os.environ.get('DB_PASSWORD')
+UPLOAD_FOLDER = '../frontend/assets/photo_gallery'
+ALLOWED_EXTENSIONS = {'jpg', 'png', 'jpeg'}
